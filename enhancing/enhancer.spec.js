585e9cfb5e997f76039a1a0c40222b6ca7bb5107
Eli Sacks <bastlifa@gmail.com>
2019-08-08 11:28:54 -0400

    filled out objects fully

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -7,7 +7,7 @@ describe('enhancer module', () =>
     {
         it('should set the durability to 100', () =>
         {
-            const item = {enhancement: 3, durability: 73}
+            const item = {name: "spear", enhancement: 3, durability: 73}
             const expectedOutput = 100
             const actualOutput = enhancer.repair({item}).durability
             expect(actualOutput).toBe(expectedOutput)
@@ -19,19 +19,19 @@ describe('enhancer module', () =>
         it('increases enhancement by 1', () =>
         {
             const expectedOutput = 5
-            const actualOutput = enhancer.succeed({enhancement: 4}).enhancement
+            const actualOutput = enhancer.succeed({name: "club", durability: 34, enhancement: 4}).enhancement
             expect(actualOutput).toBe(expectedOutput)
         })
         it('should have a cap of 20', () =>
         {
             const expectedOutput = 20
-            const actualOutput = enhancer.succeed({enhancement: 20}).enhancement
+            const actualOutput = enhancer.succeed({name: "club", durability: 34, enhancement: 20}).enhancement
             expect(actualOutput).toBe(expectedOutput)
         })
         it('should not alter the durability of the item', () =>
         {
             const expectedOutput = 43
-            const actualOutput = enhancer.succeed({durability: 43, enhancement: 4}).durability
+            const actualOutput = enhancer.succeed({name: "sword", durability: 43, enhancement: 4}).durability
             expect(actualOutput).toBe(expectedOutput)
         })
     })
@@ -41,25 +41,25 @@ describe('enhancer module', () =>
         it('should lower durability by 5, if enhancement is less than 15', () =>
         {
             const expectedOutput = 62
-            const actualOutput = enhancer.fail({enhancement: 4, durability: 67}).durability
+            const actualOutput = enhancer.fail({name: "sword", enhancement: 4, durability: 67}).durability
             expect(actualOutput).toBe(expectedOutput)
         })
         it('should lower durability by 10, if enhancement is greater than or equal to 15', () =>
         {
             const expectedOutput = 45
-            const actualOutput = enhancer.fail({enhancement: 17, durability: 55}).durability
+            const actualOutput = enhancer.fail({name: "sword", enhancement: 17, durability: 55}).durability
             expect(actualOutput).toBe(expectedOutput)
         })
         it('should lower enhancement by 1, if enhancement is greater than 16', () =>
         {
             const expectedOutput = 16
-            const actualOutput = enhancer.fail({enhancement: 17, durability: 55}).enhancement
+            const actualOutput = enhancer.fail({name: "sword", enhancement: 17, durability: 55}).enhancement
             expect(actualOutput).toBe(expectedOutput)
         })
         it('should keep enhancement the same, if enhancement is less than or equal to 16', () =>
         {
             const expectedOutput = 16
-            const actualOutput = enhancer.fail({enhancement: 16, durability: 55}).enhancement
+            const actualOutput = enhancer.fail({name: "sword", enhancement: 16, durability: 55}).enhancement
             expect(actualOutput).toBe(expectedOutput)
         })
     })
